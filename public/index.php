edd29f216d060bb1660c02b0356fdce46905c32c
panucha <panucha.p@i3gateway.com>
2026-07-18 13:53:49 +0700

    Fix base path resolution in index.php for improved application loading

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$basePath = is_file(__DIR__.'/../vendor/autoload.php')
+    ? __DIR__.'/..'
+    : __DIR__.'/../laravel-app';
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
@@ -16,7 +20,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -31,7 +35,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require $basePath.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +48,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
